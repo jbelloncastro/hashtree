17fdaeca842eda051b44b3a0f780b251536a36a2
Jorge Bellon <jorge.bellon@bsc.es>
2016-06-16 18:39:59 +0200

    Added setUp and tearDown functions for random insertion test
    Random number generation is no longer taken into account in
    performance measurements

--- test/test_insert_rand.cpp
+++ test/test_insert_rand.cpp
@@ -10,27 +10,7 @@
 #include <random>
 #include "../hash_trie.hpp"
 
-typedef std::chrono::high_resolution_clock hrclock_t;
-typedef hrclock_t::time_point ticks_t;
-
-//struct atomic_unsigned 
-//{
-//    atomic_unsigned() : _value(0) {}
-//    atomic_unsigned( const atomic_unsigned& o ) : _value( o._value.load() ) {}
-//
-//    unsigned int operator++(int)
-//    {
-//        return _value.fetch_add(1);
-//    }
-//
-//    atomic_unsigned& operator+=(unsigned int value )
-//    {
-//        _value.fetch_add(value);
-//        return *this;
-//    }
-//
-//    std::atomic<unsigned int> _value;
-//};
+uint64_t* values;
 
 struct atomic_unsigned : std::atomic<unsigned int> 
 {
@@ -38,32 +18,48 @@ struct atomic_unsigned : std::atomic<unsigned int>
     atomic_unsigned( const atomic_unsigned& ref ) { this->store( ref.load() ); }
 };
 
+typedef std::unordered_map< uint64_t, atomic_unsigned > UnorderedMap;
+typedef unordered::hash_trie< uint64_t, atomic_unsigned > HashTrie;
+typedef std::chrono::high_resolution_clock hrclock_t;
+typedef hrclock_t::time_point ticks_t;
+
 constexpr double elapsed( const ticks_t& ini, const ticks_t& end ) {
    return std::chrono::duration_cast< std::chrono::duration<double> >( end - ini ).count();
 }
 
 void usage( const char argv0[] ) {
-   std::cout << argv0 << " number_of_insertions" << std::endl;
+   std::cout << argv0 << " number_of_insertions range_size" << std::endl;
    exit( 0 );
 }
 
+void setUp( size_t insertions, size_t range_size ) {
+   std::default_random_engine generator;
+   // Param 1: mean, Param 2: standard deviation
+   std::uniform_int_distribution<uint64_t> distribution(0, range_size);
+   values = (uint64_t*)malloc( insertions * sizeof(uint64_t) );
+
+   #pragma omp parallel for shared(values)
+   for( size_t i = 0; i < insertions; ++i ) {
+      values[i] = distribution(generator);
+   }
+}
+
+void tearDown() {
+   free( values );
+}
+
 template <class Container>
-void runTest( unsigned long insertions, bool critical ) {
+void runTest( size_t insertions, bool critical ) {
    std::cout << "----------- Starting Benchmark -----------" << std::endl;
    Container map;
-
-   std::default_random_engine generator;
-   //double insertions_double = (double) insertions;
-   // Param 1: mean, Param 2: standard deviation
-   std::binomial_distribution<unsigned long> distribution(insertions, 0.5);
 
    ticks_t t0 = hrclock_t::now();
    /* ----- Perform insertions ----- */
    if(critical) {
        std::cout << "CRITICAL" << std::endl;
        #pragma omp parallel for shared( map )
-       for ( unsigned long i = 0; i < insertions; ++i ) {
-           unsigned long index = distribution(generator);
+       for ( size_t i = 0; i < insertions; ++i ) {
+           uint64_t index = values[i];
            #pragma omp critical ///< Since the values are atomic the critical is only needed if the map is not thread-safe
            map[index]++;
        }
@@ -71,18 +67,17 @@ void runTest( unsigned long insertions, bool critical ) {
    else {
        std::cout << "NOT CRITICAL" << std::endl;
        #pragma omp parallel for shared( map )
-       for ( unsigned long i = 0; i < insertions; ++i ) {
-           unsigned long index = distribution(generator);
+       for ( size_t i = 0; i < insertions; ++i ) {
+           uint64_t index = values[i];
            map[index]++;
        }
    }
    /* --- END Perform insertions --- */
    ticks_t t1 = hrclock_t::now();
    /* ----- Check result ----- */
-   unsigned long check = 0;
+   uint64_t check = 0;
    for ( auto it = map.begin(); it != map.end(); ++it ) {
         check+=it->second; 
-        //std::cout << "Check from " << check-it->second << " to " << check << " because it->second is " << it->second << std::endl;
    }
    if( check != insertions )
        std::cout << "Check failed. Expecting " << insertions << " but got " << check << "." << std::endl;
@@ -95,15 +90,21 @@ void runTest( unsigned long insertions, bool critical ) {
 }
 
 int main( int argc, char* argv[] ) {
-   if ( argc < 2 ) {
+   if ( argc < 3 ) {
       usage( argv[0] );
    }
-   unsigned long insertions;
+   size_t insertions, range_size;
    sscanf(argv[1], "%lu", &insertions);
+   sscanf(argv[2], "%lu", &range_size);
+   std::cout << "Insertions: " << insertions
+             << " Range: " << range_size << std::endl;
+   setUp( insertions, range_size );
    std::cout << "---------- UNORDERED MAP ----------" << std::endl;
-   runTest< std::unordered_map< unsigned long, atomic_unsigned > >( insertions, true );
+   runTest<UnorderedMap>( insertions, true );
    std::cout << "---------- HASH TRIE ----------" << std::endl;
-   runTest< unordered::hash_trie< unsigned long, atomic_unsigned > >( insertions, false );
+   runTest<HashTrie>( insertions, false );
    //std::cout << "---------- HASH TRIE WITH CRITICAL ----------" << std::endl;
    //runTest< unordered::hash_trie< unsigned long, atomic_unsigned > >( insertions, true );
+   tearDown();
+   return 0;
 }
